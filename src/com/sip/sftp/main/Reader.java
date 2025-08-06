ac44648433686b38acce446d97271f0a3386e292
Myat Min Ko <myatminko@startinpoint.com>
2025-08-06 09:19:43 +0630

    copy and move files from one folder to another folder on the server

--- src/com/sip/sftp/main/Reader.java
+++ src/com/sip/sftp/main/Reader.java
@@ -13,19 +13,31 @@ public class Reader {
 	public static void main(String[] args) {
 		
 		SFTPUtils sftpUtils = new SFTPUtils();
-		String path = "/UOBK_BounceMail_ContentRepo/HYMTest";
+//		String path = "/UOBK_BounceMail_ContentRepo/DoneClone";
+		String sourcePath = "/UOBK_BounceMail_ContentRepo/DoneClone/";
+		String TargetPath = "/UOBK_BounceMail_ContentRepo/Failed/";
 		ChannelSftp channelSftp = sftpUtils.connect();
 		
-		boolean isDirExit = sftpUtils.isDirectoryExist(channelSftp, path);
+		boolean isDirExit = sftpUtils.isDirectoryExist(channelSftp, sourcePath);
 
 		if(isDirExit) {
-			System.out.println("Given directory ( " + path + " ) is exist.");
+			System.out.println("Given directory ( " + sourcePath + " ) is exist.");
 			try {
-				Vector<LsEntry> files = channelSftp.ls(path);
+				Vector<LsEntry> files = channelSftp.ls(sourcePath);
 				if(files.size() > 0) {
 					System.out.println("Total files: "+ files.size());
 					for(LsEntry file: files) {
 						System.out.println("file name: " + file.getFilename());
+						if(file.getFilename().toLowerCase().endsWith(".xml")) {
+							
+							System.out.println("xml file: " + sourcePath + file.getFilename());
+							sftpUtils.moveFile(channelSftp, sourcePath + file.getFilename(), TargetPath + file.getFilename());
+						}
+						if(file.getFilename().toLowerCase().endsWith(".pdf")) {
+							
+							System.out.println("pdf file: " + sourcePath + file.getFilename());
+							sftpUtils.copyFile(channelSftp, sourcePath + file.getFilename(), TargetPath + file.getFilename());
+						}
 					}
 				} 
 				else {
@@ -39,7 +51,7 @@ public class Reader {
 			}
 		}
 		else {
-			System.out.println("Given directory ( " + path + " ) is not exist.");
+			System.out.println("Given directory ( " + sourcePath + " ) is not exist.");
 		}
 	}
 
--- src/com/sip/sftp/utils/SFTPUtils.java
+++ src/com/sip/sftp/utils/SFTPUtils.java
@@ -1,5 +1,7 @@
 package com.sip.sftp.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.Properties;
 
 import com.jcraft.jsch.Channel;
@@ -12,63 +14,85 @@ public class SFTPUtils {
 
 	String sftpHost = "192.168.3.109";
 	int sftpPort = 22;
-    String sftpUser = "rpauser";
-    String privateKeyPath = "C:\\SFTP KEY FILE\\UTradeX_ppk\\puttykeygen_UTradeX_4096.ppk";
+	String sftpUser = "rpauser";
+	String privateKeyPath = "C:\\SFTP KEY FILE\\UTradeX_ppk\\puttykeygen_UTradeX_4096.ppk";
 
-    private Session session;
-    private ChannelSftp channelSftp;
+	private Session session;
+	private ChannelSftp channelSftp;
 
-    public ChannelSftp connect() {
-        try {
-            JSch jsch = new JSch();
-            jsch.addIdentity(privateKeyPath);
+	public ChannelSftp connect() {
+		try {
+			JSch jsch = new JSch();
+			jsch.addIdentity(privateKeyPath);
 
-            session = jsch.getSession(sftpUser, sftpHost, sftpPort);
+			session = jsch.getSession(sftpUser, sftpHost, sftpPort);
 
-            Properties config = new Properties();
-            config.put("StrictHostKeyChecking", "no");
-            session.setConfig(config);
+			Properties config = new Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
 
-            session.connect();
-            System.out.println("Session connected successfully.");
+			session.connect();
+			System.out.println("Session connected successfully.");
 
-            Channel channel = session.openChannel("sftp");
-            channel.connect();
-            channelSftp = (ChannelSftp) channel;
+			Channel channel = session.openChannel("sftp");
+			channel.connect();
+			channelSftp = (ChannelSftp) channel;
 
-            System.out.println("SFTP Channel connected successfully.");
-            return channelSftp;
+			System.out.println("SFTP Channel connected successfully.");
+			return channelSftp;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            disconnect(); 
-            return null;
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			disconnect();
+			return null;
+		}
+	}
 
-    public void disconnect() {
-        if (channelSftp != null && channelSftp.isConnected()) {
-            channelSftp.exit();
-            System.out.println("SFTP channel closed.");
-        }
-        if (session != null && session.isConnected()) {
-            session.disconnect();
-            System.out.println("SSH session disconnected.");
-        }
-    }
-    
-    public boolean isDirectoryExist(ChannelSftp channelSftp, String path) {
-    	
-    	try {
+	public void disconnect() {
+		if (channelSftp != null && channelSftp.isConnected()) {
+			channelSftp.exit();
+			System.out.println("SFTP channel closed.");
+		}
+		if (session != null && session.isConnected()) {
+			session.disconnect();
+			System.out.println("SSH session disconnected.");
+		}
+	}
+
+	public boolean isDirectoryExist(ChannelSftp channelSftp, String path) {
+
+		try {
 			channelSftp.cd(path);
 			return true;
 		} catch (SftpException e) {
 			// TODO Auto-generated catch block
 			return false;
 		}
-    	
-    }
-    
-    
-    
+
+	}
+
+	public void copyFile(ChannelSftp channelSftp, String sourcePath, String copyTargetPath) throws SftpException {
+
+		//download file to memory
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		channelSftp.get(sourcePath, outputStream);
+		
+		//copy file to target folder on the sftp server
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+		channelSftp.put(inputStream, copyTargetPath);
+		
+		System.out.println("File copied from " + sourcePath + " to " + copyTargetPath);
+		
+	}
+
+	public void moveFile(ChannelSftp channelSftp, String sourcePath, String moveTargetPath) throws SftpException {
+
+		copyFile(channelSftp, sourcePath, moveTargetPath);
+		
+		channelSftp.rm(sourcePath);
+		
+		System.out.println("File moved from " + sourcePath + " to " + moveTargetPath);
+		
+	}
+
 }
